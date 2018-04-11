068f41cf636425f7fafa2ab911b7e6e7083608ab
Siddhant Singhal <singhal.si@husky.neu.edu>
2018-04-11 17:54:22 -0400

    added constants

---
+++ src/main/java/neu/algos/proj/ga/Driver.java
@@ -0,0 +1,14 @@
+package neu.algos.proj.ga;
+
+public class Driver {
+
+    public static final int POPULATION_SIZE = 9;
+    public static final double MUTATION_RATE = 0.01;
+    public static final double CROSSOVER_RATE = 0.9;
+    public static final int TOURNAMENT_SELECTION_SIZE = 3;
+    public static final int ELITE_SCHEDULES_COUNT = 2;
+
+    public static void main(String[] args){
+
+    }
+}
