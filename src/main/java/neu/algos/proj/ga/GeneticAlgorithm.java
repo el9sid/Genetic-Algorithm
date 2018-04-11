115f0df4d7b5dc825e11e89942b484fcad4ae683
Siddhant Singhal <singhal.si@husky.neu.edu>
2018-04-11 17:49:24 -0400

    added method definitions

--- src/main/java/neu/algos/proj/ga/GeneticAlgorithm.java
+++ src/main/java/neu/algos/proj/ga/GeneticAlgorithm.java
@@ -3,4 +3,40 @@ package neu.algos.proj.ga;
 public class GeneticAlgorithm {
 
     //Evoloves the population from one generation to other using crossover.
+
+    private Data data;
+
+    public GeneticAlgorithm(Data data) {
+        this.data = data;
+    }
+
+    public Population crossoverPopulation(Population population) {
+
+        return null;
+    }
+
+    public Population selectTournamentPopulation(Population population) {
+
+        return null;
+    }
+
+    public Population mutatePopulation(Population population) {
+
+        return null;
+    }
+
+    public Schedule crossoverSchedule(Schedule schedule1, Schedule schedule2) {
+
+        return null;
+    }
+
+    public Schedule mutateSchedule(Schedule scheduleMutation){
+
+        return null;
+    }
+
+    public Population evolvePopulation(Population population){
+
+        return mutatePopulation(population);
+    }
 }
