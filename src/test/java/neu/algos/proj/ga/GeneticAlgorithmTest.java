43a4a3eef64a7378b61885e5b9bbcc8f6aaf62b3
Siddhant Singhal <singhal.si@husky.neu.edu>
2018-04-14 19:02:04 -0400

    added test cases for genetic algorithm verification

--- src/test/java/neu/algos/proj/ga/GeneticAlgorithmTest.java
+++ src/test/java/neu/algos/proj/ga/GeneticAlgorithmTest.java
@@ -6,16 +6,29 @@ import static org.junit.Assert.*;
 
 public class GeneticAlgorithmTest {
 
+    Data data = new Data();
+    GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(data);
+
+
     @Test
     public void crossoverPopulation() {
+        Population p1 = new Population(100, data);
+        Population p2 = geneticAlgorithm.crossoverPopulation(p1);
+        assertNotSame(p2,p1);
     }
 
     @Test
     public void selectTournamentPopulation() {
+        Population p1 = new Population(100, data);
+        Population p2 = geneticAlgorithm.selectTournamentPopulation(p1);
+        assertNotSame(p2,p1);
     }
 
     @Test
     public void mutatePopulation() {
+        Population p1 = new Population(200, data);
+        Population p2 = geneticAlgorithm.mutatePopulation(p1);
+        assertNotSame(p2,p1);
     }
 
     @Test
